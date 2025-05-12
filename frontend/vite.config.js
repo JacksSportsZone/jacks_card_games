ef21b75194806fe22937971ae9c0c433fc04faf2
Jack Frambes <jframbeees@gmail.com>
2025-05-12 14:36:40 -0400

    Added server routing for different applications and added basic website for server and cardgames

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
 
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: path.resolve(__dirname, '../static'),
+    outDir: 'build',
     emptyOutDir: true
   }
 });
